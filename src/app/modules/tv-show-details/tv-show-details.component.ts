7eededc213782c90c030ee676f758f3bcccf029a
Anne-Laure CHARLES <anne-laure-charles@orange.fr>
2023-07-31 21:56:22 +0200

    Exo 3 : add standalone component.

--- src/app/modules/tv-show-details/tv-show-details.component.ts
+++ src/app/modules/tv-show-details/tv-show-details.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tv-show-details',
+  standalone: true,
   templateUrl: './tv-show-details.component.html',
   styleUrls: ['./tv-show-details.component.css']
 })
